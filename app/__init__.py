b09c87063623d16059b755e05a60098e17fe26ee
Artem Botkin <a.botkin@ngenix.net>
2025-08-04 05:21:43 +0300

    upd

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 import os
 
+STATIC_BASE_URL = 'https://static.abotkin.space'
+
 
 def create_app():
     app = Flask(__name__,  static_folder=None)
+
+    @app.context_processor
+    def inject_static_url():
+        return {'static_url': STATIC_BASE_URL}
 
     # Конфигурация
     app.config['UPLOAD_FOLDER'] = os.path.join(
